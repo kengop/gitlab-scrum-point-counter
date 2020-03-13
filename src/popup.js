75bf8fb5bce4b2d77a62bbe6fc77aec93a305369
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2020-03-14 08:39:47 +0900

    Apply Bootstrap theme
    - title → status

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const countPoint = document.getElementById('countPoint');
+const resultTable = document.getElementById('resultTable');
 const result = document.getElementById('result');
+
+resultTable.setAttribute('hidden', true);
 
 countPoint.onclick = function(_element) {
   chrome.tabs.query({active: true, currentWindow: true}, function(_tabs) {
@@ -9,19 +12,27 @@ countPoint.onclick = function(_element) {
         file: 'src/contentScript.js'
     });
   });
-  result.textContent = 'now counting...';
+  countPoint.textContent = 'now counting...';
+  while (result.firstChild) result.removeChild(result.firstChild);
 };
 
 chrome.runtime.onMessage.addListener(
   function(message, _sender, sendResponse) {
     if (message.event == 'scriptEnd'){
-      let resultText = '';
       for (const boardKey of Object.keys(message.value)) {
         const board = message.value[boardKey];
-        resultText += board.title + ': ' + board.point + '\n';
+        var row = document.createElement('tr');
+        var status = document.createElement('td');
+        status.textContent = board.status;
+        var point = document.createElement('td');
+        point.textContent = board.point;
+        row.appendChild(status);
+        row.appendChild(point);
+        result.appendChild(row);
       }
-      result.textContent = resultText;
+      if (resultTable.hasAttribute('hidden')) resultTable.removeAttribute('hidden');
     }
+    countPoint.textContent = 'Count again！';
     sendResponse({result: "ok"});
   }
 );
